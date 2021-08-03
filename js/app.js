fd5814edaab3ebe6a07ad8156db2c6ec94e06d13
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-03 13:07:55 -0300

    edit function added with some bugs

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 // Variables
 const form = document.querySelector('#form');
 const tasksList = document.querySelector('#list');
-let tasks = [];
+var tasks = [];
+var edit = false;
 
+var taskObj = {
+    id: '',
+    task: ''
+}
 
 // Event Listeners
 eventListeners();
 
 function eventListeners(){
-    // Carga tweet al enviar formulario
+    // Carga tweet al enviar form
     form.addEventListener('submit', addTask);
 
     // Carga tweets cuando el documento está listo
@@ -21,41 +26,57 @@ function eventListeners(){
 
 // Funciones
 
-// Agrega task al enviar formulario
+// Agrega task al enviar form
 function addTask(e){
     e.preventDefault();
 
     const task = document.querySelector('#todolist').value;
 
     if (task === ''){
-        showError('Task cannot be empty');
+        showAlert('Task cannot be empty', 'error');
         return;
     }
 
-    const taskObj = {
-        id: Date.now(),
-        task
+    if(edit) {
+        // Estamos editando
+        taskObj.task = task;
+        taskEditor( {...taskObj} );
+
+        showAlert('Modified successfully', 'success');
+
+        //form.value = 'Add Task';
+
+        edit = false;
+
+    } else {
+
+        // Generar un ID único
+        taskObj = {
+            id: Date.now(),
+            task
+        }
+
+        tasks =[...tasks, taskObj];
+
+        // Mostrar mensaje de que todo esta bien...
+        showAlert('Added successfully', 'success');
     }
 
-    // Se agrega ultima task al arreglo
-    tasks =[...tasks, taskObj];
-
     createHTML();
-
-    // Reiniciar formulario
     form.reset();
+    console.log(tasks);
 }
 
 // Muestra error recibido por parametro
-function showError(error){
-    const errorMsg = document.createElement('p');
-    errorMsg.textContent = error;
-    errorMsg.classList.add('error');
+function showAlert(message, type){
+    const alertMsg = document.createElement('p');
+    alertMsg.textContent = message;
+    alertMsg.classList.add(type);
 
-    form.appendChild(errorMsg);
+    form.appendChild(alertMsg);
 
     setTimeout(() => {
-        errorMsg.remove();
+        alertMsg.remove();
     }, 3000);
 }
 
@@ -65,22 +86,39 @@ function createHTML(){
 
     if (tasks.length > 0){
         tasks.forEach( task => {
-            // Boton para eliminar
+            // Delete Button
             const deleteBtn = document.createElement('a');
-            deleteBtn.classList.add('borrar-tweet');
+            deleteBtn.classList.add('delete-task');
             deleteBtn.innerText = 'X';
 
-            // Funcion eliminar
-            deleteBtn.onclick = () => {
-                borrarTweet(task.id);
-            }
+            deleteBtn.onclick = () => deleteTask(task.id);
 
-            // Crea Lista de tweets
-            const li = document.createElement('li');
+            // Edit Button
+            const editBtn = document.createElement('a');       
+            editBtn.classList.add('edit-task');
+            editBtn.innerHTML = 'Edit';
+
+            editBtn.onclick = () => editTask(task);
+
+            // Separate
+            const separate = document.createElement('a');
+            separate.textContent = `            `;
+            separate.classList.add('edit-task');
+
+
+            
+            const li = document.createElement('li');            
+            li.classList.add ('checkbox');
             li.innerText = task.task;
+            //li.innerHTML = `<input type="checkbox"> ${task.task}`;
+            
             li.appendChild(deleteBtn);
+            li.appendChild(separate);
+            li.appendChild(editBtn);
+            
 
             tasksList.appendChild(li);
+            
         })
     }
 
@@ -99,7 +137,37 @@ function syncStorage(){
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function borrarTweet(id){
+function deleteTask(id){
     tasks = tasks.filter(task => task.id !== id);
+    showAlert('Task deleted', 'success');
     createHTML();
+}
+
+function taskEditor(newTask) {
+    tasks = tasks.map( task => task.id === newTask.id ? newTask : task);
+}
+
+
+function editTask(eTask){
+    const {task, id} = eTask;
+    
+    // Reiniciar el objeto
+    taskObj.task = task;
+    taskObj.id = id;
+    console.log(taskObj.id);
+
+    // Llenar los inputs
+    document.querySelector('#todolist').value = task;
+    //propietarioInput.value = propietario;
+
+
+    // Cambiar texto del botón
+    //form.value = 'Save Changes';
+
+    edit = true;
+}
+
+function resetObj() {
+    taskObj.task = '';
+    taskObj.id = '';
 }
